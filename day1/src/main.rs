cb349a501b7808fb7cd93a5f6ee682c3e06d47d7
premiumbeef <howard_teo@hotmail.com>
2023-01-22 20:05:47 -0800

    Completed part 2 of Day 1 to find the sum of the Top 3 elf with the most calories.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -13,6 +13,13 @@ fn main() {
     // collect counts as a vector. <_> can automatically identify what type is being returned
     let mut v:Vec<_> = counts.collect();
     v.sort();
-    let largest_num = v.len() - 1;
-    println!("Largest calories is {}\n", v[largest_num]);
+
+    // Part 2: find the sum of the top 3 
+    let mut total_sum = 0;
+    for n in 1..=3 {
+        let largest_num = v.len() - n;
+        total_sum += v[largest_num];
+        println!("Top {} calories is {}\n", n, v[largest_num]);
+    }
+    println!("Total of top 3 calories is {}\n", total_sum);
 }
